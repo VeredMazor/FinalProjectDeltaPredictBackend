bc39eac976ceab767abcb3ec3f4bf1ee53fa588e
VeredMazor <veredm582@gmail.com>
2022-08-27 15:58:25 +0300

    secend commit

--- Server/FlaskServer.py
+++ Server/FlaskServer.py
@@ -3,17 +3,21 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from flask import Flask, render_template
+from flask import Flask, render_template #impor inctens of flask
 from flask import request
 from waitress import serve
-#from flask_cors import CORS
+from flask_cors import CORS, cross_origin
+import datetime
 
-app = Flask (__name__)
+
+x = datetime.datetime.now()
+
+app = Flask (__name__) #becuase the instanc we can call flask and put the name val.
 app._static_folder = ''
 
-#CORS(app)
+CORS(app)
 
-@app.route("/")
+@app.route("/") #Decorator in python, its basically saying that what url in your website i am going to navigate through and display you some html code.
 @app.route('/data')
 def get_time():
     # Returning an api for showing in  reactjs
@@ -24,14 +28,11 @@ def get_time():
         "programming": "python"
     }
 
-import datetime
-
-x = datetime.datetime.now()
 
 
 if __name__ == "__main__":
-   # app.run(debug=True)
-    serve(app, host="0.0.0.0", port=8080)
+   app.run(debug=True)
+    #serve(app, host="0.0.0.0", port=8080)
 
 
 
